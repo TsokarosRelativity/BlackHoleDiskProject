1ddf8fa9a784d1cab38ef79afca8948f5e35ff45
sjammi2 <sjammi2@illinois.edu>
2024-10-03 22:42:50 -0500

    added relevant papers

---
+++ TestCases/InterpolationTestCases.py
@@ -0,0 +1,12 @@
+import unittest
+from InitialData.interpolate import *
+from InitialData.interpolate_grid import *
+
+
+class TestInterpolation(unittest.TestCase):
+    def InterpolationTest(self):
+        """
+        Test the interpolation functions
+        """
+        return
+
