89341cdc73ce19602e6a5adbf3cec3f598511b4f
sjammi2 <sjammi2@illinois.edu>
2024-10-22 17:34:16 -0500

    routine 7

---
+++ InitialData/routine7/interpolate.py
@@ -0,0 +1,130 @@
+import numpy as np
+import pandas as pd
+
+#### BH Horizon Radius r_s = 0.5 * np.sqrt(kerrm**2 - kerra**2)
+kerrm = 1
+kerra = 0.8
+r_s = 0.5 * np.sqrt(kerrm**2 - kerra**2)
+
+def locate_point(
+    r_arr, theta_arr, phi_arr, p
+):  # r_arr, theta_arr, phi_arr define the ID grid, p defines a point in the new grid
+    # p should be a tuple (x,y,z) in cartesian coords
+    np.append(phi_arr[-2], np.append(phi_arr, phi_arr[1]))
+    x, y, z = p
+    r_0 = np.sqrt(x**2 + y**2 + z**2)
+    theta_0 = np.arccos(z / r_0)
+    skip = False
+    phi_0 = np.arctan2(y, x)
+    phi_0 = phi_0 % (2 * np.pi)  # ensures phi takes values between [0,2pi]
+    rs, ts, ps = [], [], []
+    
+    for i, phi in enumerate(phi_arr[1:-1]):
+        if phi_0 == phi:
+            ps = [phi_arr[i], phi_arr[i+2]]
+            break
+        if phi_arr[i+1] < phi_0 < phi_arr[i+2]:
+            ps = [phi_arr[i+1], phi_arr[i+2]]
+            break
+    
+    if theta_0 == theta_arr[0]:
+        ts = [theta_arr[1]]
+        skip = True
+        ps.append([phi_arr[-2] - ps[0], phi_arr[-2] - ps[1]])
+    if theta_0 == theta_arr[-1]:
+        ts = [theta_arr[-2]]
+        ps.append([phi_arr[-2] - ps[0], phi_arr[-2] - ps[1]])
+        skip = True
+    if not skip:
+        for i, theta in enumerate(theta_arr):
+            if theta_0 == theta:
+                ts = (theta_arr[i-1], theta_arr[i+1])
+                break
+            if theta_arr[i] < theta_0 < theta_arr[i+1]:
+                ts = (theta_arr[i], theta_arr[i+1])
+                break
+
+    for i, r in enumerate(r_arr):
+        if r_0 == r:
+            rs = (r_arr[i-1], r_arr[i+1])
+            break
+        if r_arr[i] < r_0 < r_arr[i+1]:
+            rs = (r_arr[i], r_arr[i+1])
+            break
+
+    combinations = []
+    if rs[0] == 0:
+        combinations.append([0, 0, 0])
+        for tmpt in ts:
+            for tmpp in ps:
+                combinations.append([rs[1], tmpt, tmpp])
+
+    else:
+        for tmpr in rs:
+            for tmpt in ts:
+                for tmpp in ps:
+                    combinations.append([tmpr, tmpt, tmpp])
+
+
+
+    return np.array(combinations)
+#    for i in range(len(r_arr) - 1):
+#        if r_arr[i] <= r_0 <= r_arr[i + 1]:
+#            rs = (r_arr[i], r_arr[i + 1])
+#            break
+#    for i in range(len(theta_arr) - 1):
+#        if theta_arr[i] <= theta_0 <= theta_arr[i + 1]:
+#            ts = (theta_arr[i], theta_arr[i + 1])
+#            break
+#    for i in range(len(phi_arr) - 1):
+#        if phi_arr[i] <= phi_0 <= phi_arr[i + 1]:
+#            ps = (phi_arr[i], phi_arr[i + 1])
+#            break
+
+
+#    combinations = []
+#    if rs[0] == np.float64(0):
+#        combinations.append([np.float64(0), np.float64(0), np.float64(0)])
+#        for t in ts:
+#            for p in ps:
+#                combinations.append([rs[1], t, p])
+#    else:
+#        for r in rs:
+#            for t in ts:
+#                for p in ps:
+#                    combinations.append([r, t, p])
+#
+#    return np.array(combinations)
+
+
+def calc_weighted_average(points, scalar_vals, p):
+    d = np.sum((points - p) ** 2, axis=1)
+    if any(d <= 1e-4):
+        return scalar_vals[np.argmin(d)]
+    weights = 1 / ((d) ** 2)
+    weights = weights.reshape((-1, 1))
+    return np.sum(weights * scalar_vals, axis=0) / np.sum(weights)
+
+
+def spherical2cart(r, t, p):
+    x = r * np.sin(t) * np.cos(p)
+    y = r * np.sin(t) * np.sin(p)
+    z = r * np.cos(t)
+    return (x, y, z)
+
+
+def interpolate_point(p, df, rad_arr, theta_arr, phi_arr, idx_point_map):
+    surroundingpoints = locate_point(rad_arr, theta_arr, phi_arr, p)  # numpy array
+    carpointdata = []
+    scalardata = []
+    for point in surroundingpoints:
+        tmpr, tmpt, tmpp = point
+        rowIndex = idx_point_map[(tmpr, tmpt, tmpp)]
+        rowdata = df.iloc[rowIndex]
+        tmpx, tmpy, tmpz = spherical2cart(tmpr, tmpt, tmpp)
+        carpointdata.append([tmpx, tmpy, tmpz])
+        scalardata.append(rowdata[3:])
+    carpointdata = np.array(carpointdata)
+    scalardata = np.array(scalardata)
+    interpolateddata = calc_weighted_average(carpointdata, scalardata, p)
+    return np.hstack((p, interpolateddata))
