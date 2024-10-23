0b5ede5189e2d66fbfbd8c52735552d09c004de7
sjammi2 <sjammi2@illinois.edu>
2024-10-23 02:56:42 -0500

    popo

--- InitialData/routine7/interpolate.py
+++ InitialData/routine7/interpolate.py
@@ -22,71 +22,108 @@ def locate_point(
     r_0 = np.sqrt(x**2 + y**2 + z**2)
     theta_0 = np.arccos(z / r_0)
     skip = False
+    origin = False
     phi_0 = np.arctan2(y, x)
     phi_0 = phi_0 % (2 * np.pi)  # ensures phi takes values between [0,2pi]
     ts = np.zeros(2, dtype=np.float64)
-    ps = np.zeros(2, dtype=np.float64)
+    ps = np.zeros(4, dtype=np.float64)
     rs = np.zeros(2, dtype=np.float64)
     ep = np.array([phi_arr[-2], phi_arr[1]])
+    tcount = 0
+    pcount = 0
+    rcount = 2
 
     for i in prange(1, len(phi_arr_extended) - 1):
         if phi_0 == phi_arr_extended[i]:
             ps[0] = phi_arr_extended[i - 1]
             ps[1] = phi_arr_extended[i + 1]
+            pcount += 2
             break
         if phi_arr_extended[i] < phi_0 < phi_arr_extended[i + 1]:
             ps[0] = phi_arr_extended[i]
             ps[1] = phi_arr_extended[i + 1]
+            pcount += 2
             break
 
     if theta_0 == theta_arr[0]:
-        ts = [theta_arr[1]]
+        ts[0] = theta_arr[1]
+        tcount += 1
         skip = True
     if theta_0 == theta_arr[-1]:
-        ts = [theta_arr[-2]]
+        ts[0] = theta_arr[-2]
+        tcount +=1
         skip = True
     if skip:
-        tmp = np.zeros(4, dtype=np.float64)
-        tmp[0] = ps[0]
-        tmp[1] = ps[1]
-        tmp[2] = ep[0]
-        tmp[3] = ep[1]
-        ps = tmp
+        ps[2] = ep[0] - ps[0]
+        ps[3] = ep[1] - ps[1]
+        pcount += 2
+
     if not skip:
         for i in prange(len(theta_arr)):
             if theta_0 == theta_arr[i]:
                 ts[0] = theta_arr[i - 1]
                 ts[1] = theta_arr[i + 1] 
+                tcount += 2
                 break
             if theta_arr[i] < theta_0 < theta_arr[i + 1]:
                 ts[0] = theta_arr[i]
                 ts[1] = theta_arr[i + 1]
+                tcount += 2
                 break
 
     for i in prange(len(r_arr)):
         if r_0 == r_arr[i]:
             if i == 0:
-                rs = [r_arr[i], r_arr[1]]
+                rs[0] = r_arr[i]
+                rs[1] = r_arr[i + 1]
+                origin = True
                 break
             else:
-                rs = [r_arr[i - 1], r_arr[i + 1]]
+                rs[0] = r_arr[i - 1]
+                rs[1] = r_arr[i + 1]
                 break
         if r_arr[i] < r_0 < r_arr[i + 1]:
-            rs = [r_arr[i], r_arr[i + 1]]
+            rs[0] = r_arr[i]
+            rs[1] = r_arr[i + 1]
             break
-    combinations = []
+    tc = 8
+    if origin:
+        tc = 5
+    combinations = np.zeros((tc, 3), dtype=np.float64)
 
-    if rs[0] == r_arr[0]:
-        combinations.append([np.float64(0), np.float64(0), np.float64(0)])
-        for tmpt in ts:
-            for tmpp in ps:
-                combinations.append([rs[1], tmpt, tmpp])
+    if origin:
+        combinations[0] = np.array([np.float64(0), np.float64(0), np.float64(0)])
+        if skip:
+            combinations[1] = np.array([rs[1], ts[0], ps[0]])
+            combinations[2] = np.array([rs[1], ts[0], ps[1]])
+            combinations[3] = np.array([rs[1], ts[0], ps[2]])
+            combinations[4] = np.array([rs[1], ts[0], ps[3]])
+        else:
+            combinations[1] = np.array([rs[1], ts[0], ps[0]])
+            combinations[2] = np.array([rs[1], ts[0], ps[1]])
+            combinations[3] = np.array([rs[1], ts[1], ps[0]])
+            combinations[4] = np.array([rs[1], ts[1], ps[1]])
     else:
-        for tmpr in rs:
-            for tmpt in ts:
-                for tmpp in ps:
-                    combinations.append([tmpr, tmpt, tmpp])
-    return np.array(combinations)
+        if skip:
+            combinations[0] = np.array([rs[0], ts[0], ps[0]])
+            combinations[1] = np.array([rs[0], ts[0], ps[1]])
+            combinations[2] = np.array([rs[0], ts[0], ps[2]])
+            combinations[3] = np.array([rs[0], ts[0], ps[3]])
+            combinations[4] = np.array([rs[1], ts[0], ps[0]])
+            combinations[5] = np.array([rs[1], ts[0], ps[1]])
+            combinations[6] = np.array([rs[1], ts[0], ps[2]])
+            combinations[7] = np.array([rs[1], ts[0], ps[3]])
+        else:
+            combinations[0] = np.array([rs[0], ts[0], ps[0]])
+            combinations[1] = np.array([rs[0], ts[0], ps[1]])
+            combinations[2] = np.array([rs[0], ts[1], ps[0]])
+            combinations[3] = np.array([rs[0], ts[1], ps[1]])
+            combinations[4] = np.array([rs[1], ts[0], ps[0]])
+            combinations[5] = np.array([rs[1], ts[0], ps[1]])
+            combinations[6] = np.array([rs[1], ts[1], ps[0]])
+            combinations[7] = np.array([rs[1], ts[1], ps[1]])
+
+    return combinations 
 
 
 @njit(parallel=True)
